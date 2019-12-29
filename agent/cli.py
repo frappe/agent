a1dd3e09069ffdafe4177ba3ce20e3f28dee32da
Aditya Hase <aditya@adityahase.com>
2019-12-29 15:56:50 +0530

    feat: Create a simple CLI
    setup config - Generate config.json
    setup supervisor - Generate supervisor.conf
    setup nginx - Generate nginx.conf

---
+++ agent/cli.py
@@ -0,0 +1,35 @@
+import json
+import click
+
+from agent.server import Server
+
+
+@click.group()
+def cli():
+    pass
+
+
+@cli.group()
+def setup():
+    pass
+
+
+@setup.command()
+def config():
+    config = {
+        "benches_directory": "/home/frappe/benches",
+        "name": "x.frappe.cloud",
+        "redis_port": 25025,
+        "web_port": 25052,
+    }
+    json.dump(config, open("config.json", "w"), sort_keys=True, indent=4)
+
+
+@setup.command()
+def supervisor():
+    Server().setup_supervisor()
+
+
+@setup.command()
+def nginx():
+    Server().setup_nginx()
