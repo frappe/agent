03b2da6296a6dd1ac1deda53da63da00eacbd97a
Aditya Hase <aditya@adityahase.com>
2020-01-06 15:09:59 +0530

    feat: Add Job and Step class
    Abstracts functionality of JobModel and StepModel

--- agent/job.py
+++ agent/job.py
@@ -12,6 +12,63 @@ from peewee import (
 agent_database = SqliteDatabase("jobs.sqlite3")
 
 
+
+@wrapt.decorator
+def save(wrapped, instance, args, kwargs):
+    wrapped(*args, **kwargs)
+    instance.model.save()
+
+
+class Action:
+    def success(self, data):
+        self.model.status = "Success"
+        self.model.data = json.dumps(data, default=str)
+        self.end()
+
+    def failure(self, data):
+        self.model.data = json.dumps(data, default=str)
+        self.model.status = "Failure"
+        self.end()
+
+    @save
+    def end(self):
+        self.model.end = datetime.datetime.now()
+        self.model.duration = self.model.end - self.model.start
+
+
+class Step(Action):
+    @save
+    def start(self, name, job):
+        self.model = StepModel()
+        self.model.name = name
+        self.model.job = job
+        self.model.start = datetime.datetime.now()
+        self.model.status = "Running"
+
+
+class Job(Action):
+    @save
+    def start(self):
+        self.model.start = datetime.datetime.now()
+        self.model.status = "Running"
+
+    @save
+    def enqueue(self, name, function, args, kwargs):
+        self.model = JobModel()
+        self.model.name = name
+        self.model.status = "Pending"
+        self.model.enqueue = datetime.datetime.now()
+        self.model.data = json.dumps(
+            {
+                "function": function.__func__.__name__,
+                "args": args,
+                "kwargs": kwargs,
+            },
+            default=str,
+            sort_keys=True,
+            indent=4,
+        )
+
 class JobModel(Model):
     name = CharField()
     status = CharField(
