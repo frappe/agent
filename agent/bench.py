4ac177b9a0bfeed89b2f205ca85856ee645f6ae8
Aditya Hase <aditya@adityahase.com>
2020-01-06 15:12:36 +0530

    feat: Attach Job and Step to Server and Bench

---
+++ agent/bench.py
@@ -0,0 +1,7 @@
+    @property
+    def job_record(self):
+        return self.server.job_record
+
+    @property
+    def step_record(self):
+        return self.server.step_record
--- agent/server.py
+++ agent/server.py
@@ -3,12 +3,15 @@ import os
 from jinja2 import Environment, PackageLoader
 
 from agent.base import Base
+from agent.job import Job, Step
 
 
 class Server(Base):
     def __init__(self, directory=None):
         self.directory = directory or os.getcwd()
         self.config_file = os.path.join(self.directory, "config.json")
+        self.job = None
+        self.step = None
 
     def execute(self, command, directory=None):
         return super().execute(command, directory=directory)
@@ -20,6 +23,19 @@ class Server(Base):
     def setup_supervisor(self):
         self._generate_supervisor_config()
         self._update_supervisor()
+
+
+    @property
+    def job_record(self):
+        if self.job is None:
+            self.job = Job()
+        return self.job
+
+    @property
+    def step_record(self):
+        if self.step is None:
+            self.step = Step()
+        return self.step
 
     def _generate_nginx_config(self):
         nginx_config = os.path.join(self.directory, "nginx.conf")
