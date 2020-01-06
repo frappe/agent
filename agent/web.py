f8ea717780552c9093baf9a09b9f45fde01e6b8e
Aditya Hase <aditya@adityahase.com>
2020-01-06 15:22:50 +0530

    feat: Add endpoint for creating new bench

--- agent/web.py
+++ agent/web.py
@@ -3,9 +3,58 @@ from flask import Flask, jsonify, request
 from playhouse.shortcuts import model_to_dict
 
 from agent.job import JobModel
+from agent.server import Server
 
 application = Flask(__name__)
 
+
+"""
+POST /benches
+{
+    "name": "bench-1",
+    "python": "/usr/bin/python3.6",
+    "apps": [
+        {
+            "name": "frappe",
+            "repo": "https://github.com/frappe/frappe",
+            "branch": "version-12",
+            "hash": "ada803c5b57e489bfbc2dee6292a4bcb3ff69aa0"
+        },
+        {
+            "name": "erpnext",
+            "repo": "https://github.com/frappe/erpnext",
+            "branch": "version-12",
+            "hash": "782f45ae5f272173b5daadb493d40cf7ccf131b4"
+        }
+    ],
+    "config": {
+        "background_workers": 8,
+        "error_report_email": "test@example.com",
+        "frappe_user": "frappe",
+        "gunicorn_workers": 16,
+        "mail_login": "test@example.com",
+        "mail_password": "test",
+        "mail_server": "smtp.example.com",
+        "monitor": 1,
+        "redis_cache": "redis://localhost:13000",
+        "redis_queue": "redis://localhost:11000",
+        "redis_socketio": "redis://localhost:12000",
+        "server_script_enabled": true,
+        "socketio_port": 9000,
+        "webserver_port": 8000
+    }
+}
+
+"""
+
+
+@application.route("/benches", methods=["POST"])
+def new_bench():
+    data = request.json
+    job = Server().new_bench(
+        data["name"], data["python"], data["config"], data["apps"]
+    )
+    return {"job": job}
 
 
 app = Flask(__name__)
