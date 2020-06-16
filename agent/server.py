0db4ebbf6490a60ab02a4b549718a6046904d9a3
Aditya Hase <aditya@adityahase.com>
2020-06-16 22:03:08 +0530

    perf: Skip building assets during bench init

--- agent/server.py
+++ agent/server.py
@@ -34,12 +34,12 @@ class Server(Base):
             # Unable to articulate the reasons as of now
             command = (
                 f"bench init --clone-from {clone} --clone-without-update "
-                f"--python {python} {name} --no-backups "
+                f"--python {python} {name} --no-backups --skip-assets"
             )
         else:
             command = (
                 f"bench init --frappe-branch {branch} --frappe-path {repo} "
-                f"--python {python} {name} --no-backups "
+                f"--python {python} {name} --no-backups --skip-assets"
             )
 
         return self.execute(command, directory=self.benches_directory)
