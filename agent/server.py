d573be103d3f0195aa6ff4b3fa9606714bf8cdb9
Aditya Hase <aditya@adityahase.com>
2020-06-17 12:01:33 +0530

    fix: Run bench init with --verbose flag

--- agent/server.py
+++ agent/server.py
@@ -34,12 +34,14 @@ class Server(Base):
             # Unable to articulate the reasons as of now
             command = (
                 f"bench init --clone-from {clone} --clone-without-update "
-                f"--python {python} {name} --no-backups --skip-assets"
+                "--no-backups --skip-assets --verbose "
+                f"--python {python} {name}"
             )
         else:
             command = (
                 f"bench init --frappe-branch {branch} --frappe-path {repo} "
-                f"--python {python} {name} --no-backups --skip-assets"
+                "--no-backups --skip-assets --verbose "
+                f"--python {python} {name}"
             )
 
         return self.execute(command, directory=self.benches_directory)
