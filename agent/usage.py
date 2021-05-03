7af7503dd0cd461345c4052bd775f398faf758b0
Gavin D'souza <gavin18d@gmail.com>
2021-05-03 17:26:34 +0530

    fix(usage): Dont break if exception occurs

--- agent/usage.py
+++ agent/usage.py
@@ -1,7 +1,31 @@
 import json
 import os
-from agent.server import Server
+import sys
+import traceback
 from datetime import datetime
+
+from agent.server import Server
+
+
+def cstr(text, encoding='utf-8'):
+    """Similar to frappe.utils.cstr"""
+    if isinstance(text, str):
+        return text
+    elif text is None:
+        return ''
+    elif isinstance(text, bytes):
+        return str(text, encoding)
+    else:
+        return str(text)
+
+
+def get_traceback():
+    """Returns the traceback of the Exception
+    """
+    exc_type, exc_value, exc_tb = sys.exc_info()
+    trace_list = traceback.format_exception(exc_type, exc_value, exc_tb)
+    body = "".join(cstr(t) for t in trace_list)
+    return body
 
 
 if __name__ == "__main__":
@@ -16,14 +40,18 @@ if __name__ == "__main__":
 
     for bench in server.benches.values():
         for site in bench.sites.values():
-            info.append(
-                {
-                    "site": site.name,
-                    "timestamp": str(datetime.utcnow()),
-                    "timezone": site.timezone,
-                    **site.get_usage(),
-                }
-            )
+            try:
+                info.append(
+                    {
+                        "site": site.name,
+                        "timestamp": str(datetime.utcnow()),
+                        "timezone": site.timezone,
+                        **site.get_usage(),
+                    }
+                )
+            except Exception:
+                error_log = f"ERROR [{site.name}:{time}]: {get_traceback()}"
+                print(error_log, file=sys.stderr)
 
     with open(target_file, "w") as f:
         json.dump(info, f, indent=1)
