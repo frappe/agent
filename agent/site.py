cc45366938913df4ff26fd8344d3351d3e7e4aa0
Aditya Hase <aditya@adityahase.com>
2023-04-14 23:35:43 +0530

    fix: MariaDB DB/Table naming

--- agent/site.py
+++ agent/site.py
@@ -255,7 +255,7 @@ class Site(Base):
         }.get(mode, "SELECT")
         queries = [
             f"CREATE OR REPLACE USER '{user}'@'%' IDENTIFIED BY '{password}'",
-            f"GRANT {privileges} ON *.{database} TO '{user}'@'%'",
+            f"GRANT {privileges} ON {database}.* TO '{user}'@'%'",
             "FLUSH PRIVILEGES",
         ]
         for query in queries:
