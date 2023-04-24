18138a16c809ec86d5714cf43b50640030e50018
Balamurali M <balamurali@frappe.io>
2023-04-24 18:04:46 +0530

    fix(Site): Remove unnecessary input

--- agent/site.py
+++ agent/site.py
@@ -595,10 +595,8 @@ print(">>>" + frappe.session.sid + "<<<")
 
         output = self.bench_execute("console", input=code)["output"]
         sid = re.search(r">>>(.*)<<<", output).group(1)
-        if sid == user: # case when it fails
-            output = self.bench_execute(f"browse --user {user}", input=code)[
-                "output"
-            ]
+        if sid == user:  # case when it fails
+            output = self.bench_execute(f"browse --user {user}")["output"]
             sid = re.search(r"\?sid=([a-z0-9]*)", output).group(1)
         return sid
 
