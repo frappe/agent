7c70d1a7a9c91fa2e41dceb30ad63d7d6df1a0c9
Aditya Hase <aditya@adityahase.com>
2024-02-21 12:32:22 +0530

    feat: Create subprocess and poll stdout/stderr for realtime updates

--- agent/base.py
+++ agent/base.py
@@ -3,6 +3,7 @@ import os
 import subprocess
 import traceback
 from datetime import datetime
+from functools import partial
 
 
 class Base:
@@ -29,16 +30,7 @@ class Base:
         start = datetime.now()
         data = {"command": command, "directory": directory, "start": start}
         try:
-            process = subprocess.run(
-                command,
-                check=True,
-                stdout=subprocess.PIPE,
-                stderr=subprocess.STDOUT,
-                cwd=directory,
-                shell=True,
-                input=input.encode() if input else None,
-                executable=executable,
-            )
+            output = self.run_subprocess(command, directory, input, executable)
         except subprocess.CalledProcessError as e:
             end = datetime.now()
             data.update({"duration": end - start, "end": end})
@@ -60,14 +52,59 @@ class Base:
 
         end = datetime.now()
         output = (
-            self.remove_crs(process.stdout)
-            if remove_crs
-            else process.stdout.decode().strip()
+            self.remove_crs(output) if remove_crs else output.decode().strip()
         )
         if not skip_output_log:
             self.log("Output", output)
         data.update({"duration": end - start, "end": end, "output": output})
         return data
+
+    def run_subprocess(self, command, directory, input, executable):
+        # Start a child process and start reading output immediately
+        with subprocess.Popen(
+            command,
+            stdout=subprocess.PIPE,
+            stderr=subprocess.STDOUT,
+            stdin=subprocess.PIPE if input else None,
+            cwd=directory,
+            shell=True,
+            executable=executable,
+        ) as process:
+            if input:
+                process._stdin_write(input.encode())
+
+            output = self.parse_output(process)
+            retcode = process.poll()
+            # This is equivalent of check=True
+            # Raise an exception if the process returns a non-zero return code
+            if retcode:
+                raise subprocess.CalledProcessError(
+                    retcode, command, output=output
+                )
+        return output
+
+    def parse_output(self, process):
+        lines = []
+        # This is equivalent of remove_crs
+        # Make sure output matches what'll be shown in the terminal
+        # This won't work for top, htop etc, but good enough to handle progress bars
+        if process.stdout:
+            line = ""
+            for char in iter(partial(process.stdout.read, 1), ""):
+                char = char.decode()
+                if char == "" and process.poll() is not None:
+                    break
+                elif char == "\r":
+                    # Overwrite last line
+                    if lines:
+                        lines[-1] = line
+                    line = ""
+                elif char == "\n":
+                    lines.append(line)
+                    line = ""
+                else:
+                    line += char
+        return "\n".join(lines)
 
     @property
     def config(self):
