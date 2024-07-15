c211e04bb61b07c02e6346a1dde00f9cb8f00a5f
Arun <me@arunmathaisk.in>
2024-07-15 17:22:26 +0530

    chore: minor parse issue

--- agent/site.py
+++ agent/site.py
@@ -49,7 +49,7 @@ class Site(Base):
     def bench_execute(self, command, input=None):
         return self.bench.docker_execute(
             f"bench --site {self.name} {command}", input=input
-        )
+        )["output"]
 
     def dump(self):
         return {"name": self.name}
