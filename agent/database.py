fab9dafe9b29a44be36ff03e9b3771811d5c4b4f
Arun <me@arunmathaisk.in>
2024-07-21 19:25:19 +0530

    chore: minor

--- agent/database.py
+++ agent/database.py
@@ -184,6 +184,7 @@ class DatabaseServer(Server):
     def fetch_column_stats(
         self, schema, table, private_ip, mariadb_root_password, doc_name
     ):
+        self.data["doc_name"] = doc_name
         self._fetch_column_stats(
             schema, table, private_ip, mariadb_root_password
         )
