bdafb70b23bf55c89429ede23ca6d6d900655a27
Arun Mathai <me@arunmathaisk.in>
2025-01-17 11:46:12 +0530

    fix(devbox): Pre-commit was failing. correction applied.

--- agent/patches/add_devbox_directory_config.py
+++ agent/patches/add_devbox_directory_config.py
@@ -6,7 +6,8 @@ import os
 def execute():
     """Add devbox config key for old app servers"""
     from agent.server import Server
+
     server = Server()
     config = server.config
-    config["devboxes_directory"] = os.path.join(os.path.dirname(config["benches_directory"]),"devboxes")
+    config["devboxes_directory"] = os.path.join(os.path.dirname(config["benches_directory"]), "devboxes")
     server.setconfig(config)
