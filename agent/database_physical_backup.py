c795677c5cabd6f639d3d3e159dace7033c1539f
tanmoysrt <57363826+tanmoysrt@users.noreply.github.com>
2025-02-14 10:57:42 +0530

    feat(physical-restore): Store sequence table info in snapshot

--- agent/database_physical_backup.py
+++ agent/database_physical_backup.py
@@ -50,6 +50,7 @@ class DatabasePhysicalBackup(DatabaseServer):
 
         self.innodb_tables: dict[str, list[str]] = {db: [] for db in self.databases}
         self.myisam_tables: dict[str, list[str]] = {db: [] for db in self.databases}
+        self.sequence_tables: dict[str, list[str]] = {db: [] for db in self.databases}
         self.files_metadata: dict[str, dict[str, str]] = {db: {} for db in self.databases}
         self.table_schemas: dict[str, str] = {}
 
@@ -82,7 +83,7 @@ class DatabasePhysicalBackup(DatabaseServer):
         for db_name in self.databases:
             db_instance = self.get_db(db_name)
             query = (
-                "SELECT table_name, ENGINE FROM information_schema.tables "
+                "SELECT table_name, engine, table_type FROM information_schema.tables "
                 "WHERE table_schema = DATABASE() AND table_type != 'VIEW' "
                 "ORDER BY table_name"
             )
@@ -90,10 +91,14 @@ class DatabasePhysicalBackup(DatabaseServer):
             for row in data:
                 table = row[0]
                 engine = row[1]
+                table_type = row[2]
                 if engine == "InnoDB":
                     self.innodb_tables[db_name].append(table)
                 elif engine == "MyISAM":
                     self.myisam_tables[db_name].append(table)
+
+                if table_type == "SEQUENCE":
+                    self.sequence_tables[db_name].append(table)
 
     @step("Flush Database Tables")
     def flush_tables(self):
@@ -216,6 +221,7 @@ class DatabasePhysicalBackup(DatabaseServer):
             data = {
                 "innodb_tables": self.innodb_tables[db_name],
                 "myisam_tables": self.myisam_tables[db_name],
+                "sequence_tables": self.sequence_tables[db_name],
                 "table_schema": self.table_schemas[db_name],
                 "files_metadata": self.files_metadata[db_name],
             }
