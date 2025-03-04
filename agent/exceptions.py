e07f0725785d909cce918902318975d194692f71
Balamurali M <balamurali@frappe.io>
2025-03-04 13:59:30 +0530

    fix(bench): Only validate the relevant site's config

--- agent/exceptions.py
+++ agent/exceptions.py
@@ -1,6 +1,11 @@
 from __future__ import annotations
 
 
+class AgentException(Exception):
+    def __init__(self, data):
+        self.data = data
+
+
 class BenchNotExistsException(Exception):
     def __init__(self, bench):
         self.bench = bench
@@ -16,3 +21,9 @@ class SiteNotExistsException(Exception):
         self.message = f"Site {site} does not exist on bench {bench}"
 
         super().__init__(self.message)
+
+
+class InvalidSiteConfigException(AgentException):
+    def __init__(self, data: dict, site=None):
+        self.site = site
+        super().__init__(data)
