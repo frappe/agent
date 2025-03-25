6b24eff05e5449ca096db30a3cfd4265802e42f8
Aradhya-Tripathi <aradhya@frappe.io>
2025-03-25 10:40:12 +0000

    chore: added todo

--- agent/callbacks.py
+++ agent/callbacks.py
@@ -2,5 +2,6 @@ import requests
 
 
 def callback(job, connection, result, *args, **kwargs):
+    # TODO: We might need to address this hard coding!
     press_url = "https://frappecloud.com"
     requests.post(f"{press_url}/press.api.callbacks.callback", data={"job_id": job.id})
