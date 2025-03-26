ba1f40395b2cd1a0ddaa185206983eeae616d2fb
Aradhya-Tripathi <aradhya@frappe.io>
2025-03-26 12:24:46 +0000

    fix: fixed request path

--- agent/callbacks.py
+++ agent/callbacks.py
@@ -3,5 +3,7 @@ import requests
 
 def callback(job, connection, result, *args, **kwargs):
     # TODO: We might need to address this hard coding!
-    press_url = "https://frappecloud.com"
-    requests.post(f"{press_url}/press.api.callbacks.callback", data={"job_id": job.id})
+    # press_url = "https://frappecloud.com"
+    press_url = "https://ddee-182-156-21-2.ngrok-free.app"
+    requests.post(f"{press_url}/api/method/press.api.callbacks.callback", data={"job_id": job.id})
+
