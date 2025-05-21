8c8b3587d05f5033a8226300eeead2b6a2fb8f92
Saurabh <saurabh6790@gmail.com>
2025-05-21 12:56:19 +0530

    feat: provision to use custom mariadb port

--- agent/database_server.py
+++ agent/database_server.py
@@ -260,6 +260,9 @@ class DatabaseServer(Server):
                 "message": "Failed to stop replication.",
                 "error": traceback.format_exc(),
             }
+    @property
+    def db_port(self):
+        return self.config.get("db_port") or 3306
 
     def processes(self, private_ip, mariadb_root_password):
         try:
@@ -268,7 +271,7 @@ class DatabaseServer(Server):
                 user="root",
                 password=mariadb_root_password,
                 host=private_ip,
-                port=3306,
+                port=self.db_port,
             )
             return self.sql(mariadb, "SHOW FULL PROCESSLIST")
         except Exception:
@@ -284,7 +287,7 @@ class DatabaseServer(Server):
                 user="root",
                 password=mariadb_root_password,
                 host=private_ip,
-                port=3306,
+                port=self.db_port,
             )
             return self.sql(mariadb, "SHOW VARIABLES")
         except Exception:
@@ -300,7 +303,7 @@ class DatabaseServer(Server):
                 user="root",
                 password=mariadb_root_password,
                 host=private_ip,
-                port=3306,
+                port=self.db_port,
             )
             return self.sql(
                 mariadb,
@@ -324,7 +327,7 @@ class DatabaseServer(Server):
                 user="root",
                 password=mariadb_root_password,
                 host=private_ip,
-                port=3306,
+                port=self.db_port,
             )
             for process in processes:
                 if (
@@ -352,7 +355,7 @@ class DatabaseServer(Server):
             user="root",
             password=mariadb_root_password,
             host=private_ip,
-            port=3306,
+            port=self.db_port,
         )
 
         return self.sql(
@@ -387,7 +390,7 @@ class DatabaseServer(Server):
         return self.fetch_column_stats(schema, table, private_ip, mariadb_root_password)
 
     def fetch_column_stats(self, schema, table, private_ip, mariadb_root_password):
-        db = Database(private_ip, 3306, "root", mariadb_root_password, schema)
+        db = Database(private_ip, self.db_port, "root", mariadb_root_password, schema)
         results = db.fetch_database_column_statistics(table)
         return {"output": json.dumps(results)}
 
@@ -397,7 +400,7 @@ class DatabaseServer(Server):
             user="root",
             password=mariadb_root_password,
             host=private_ip,
-            port=3306,
+            port=self.db_port,
         )
 
         if not query.lower().startswith(("select", "update", "delete")):
@@ -446,7 +449,7 @@ class DatabaseServer(Server):
 
     def _purge_binlog(self, private_ip: str, mariadb_root_password: str, to_binlog: str) -> bool:
         try:
-            mariadb = Database(private_ip, 3306, "root", mariadb_root_password, "mysql")
+            mariadb = Database(private_ip, self.db_port, "root", mariadb_root_password, "mysql")
             mariadb.execute_query(f"PURGE BINARY LOGS TO '{to_binlog}';", commit=True)
             return True
         except Exception:
