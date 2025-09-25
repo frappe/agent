3cf2073da4993aba1b0092df9a76cc0367c80dc8
Balamurali M <balamurali@frappe.io>
2025-09-25 11:01:32 +0530

    style(ruff): Sort imports
    (cherry picked from commit 9d9f85c39d591c08141fdbe3feb8df5400aa70a2)

--- agent/server.py
+++ agent/server.py
@@ -10,6 +10,10 @@ import time
 from contextlib import suppress
 from datetime import datetime
 
+from jinja2 import Environment, PackageLoader
+from passlib.hash import pbkdf2_sha256 as pbkdf2
+from peewee import MySQLDatabase
+
 from agent.application_storage_analyzer import (
     analyze_benches_structure,
     format_size,
@@ -24,9 +28,6 @@ from agent.job import Job, Step, job, step
 from agent.patch_handler import run_patches
 from agent.site import Site
 from agent.utils import get_supervisor_processes_status, is_registry_healthy
-from jinja2 import Environment, PackageLoader
-from passlib.hash import pbkdf2_sha256 as pbkdf2
-from peewee import MySQLDatabase
 
 
 class Server(Base):
