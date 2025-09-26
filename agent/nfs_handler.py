b1cb064581e28eda4b196a3d8574ace9e9525521
Aradhya-Tripathi <aradhya@frappe.io>
2025-09-26 08:36:01 +0000

    feat(nfs): Add file locks and allow removal

--- agent/nfs_handler.py
+++ agent/nfs_handler.py
@@ -1,7 +1,7 @@
 import os
 import typing
 
-from agent.job import job, step
+import filelock
 
 if typing.TYPE_CHECKING:
     from agent.server import Server
@@ -10,9 +10,14 @@ if typing.TYPE_CHECKING:
 class NFSHandler:
     def __init__(self, server: "Server"):
         self.server = server
+        self.exports_file = "/home/frappe/exports"
         self.shared_directory = "/home/frappe/nfs"
+        self.options = "rw,sync,no_subtree_check"
 
-    def update_nfs_exports_on_host(
+    def reload_exports(self):
+        self.server.execute("sudo exportfs -ra")
+
+    def add_to_acl(
         self,
         server_to_enable_mount_on: str,
         private_ip_to_enable_mount_on: str,
@@ -30,7 +35,20 @@ class NFSHandler:
             os.makedirs(server_shared_directory)
             self.server.execute(f"chown -R frappe:frappe {server_shared_directory}")
 
-        with open("/home/frappe/exports", "a+") as f:
-            f.write(f"{server_shared_directory} {private_ip_to_enable_mount_on}(rw,sync,no_subtree_check)\n")
+        lock = filelock.SoftFileLock(self.exports_file + ".lock")
 
-        self.server.execute("sudo exportfs -ra")
+        with lock.acquire(timeout=10), open(self.exports_file, "a+") as f:
+            f.write(f"{server_shared_directory} {private_ip_to_enable_mount_on}({self.options})\n")
+
+        self.reload_exports()
+
+    def remove_from_acl(self, file_system: str, private_ip_to_disable_mount_on: str):
+        """Unsubscrible a given private IP from a give file system"""
+        server_shared_directory = os.path.join(self.shared_directory, file_system)
+        remove_line = f"{server_shared_directory} {private_ip_to_disable_mount_on}({self.options})"
+
+        lock = filelock.SoftFileLock(self.exports_file + ".lock")
+        with lock.acquire(timeout=10):
+            self.server.execute(f"sed -i '\\|{remove_line}|d' {self.exports_file}")
+
+        self.reload_exports()
