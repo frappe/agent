b54cad7221dcb1cd90c9027a8cde8788e397a5f5
Aradhya-Tripathi <aradhya@frappe.io>
2025-10-24 05:12:33 +0000

    chore(redis): Update change password when changing bench directories
    (cherry picked from commit 1e473f7f4e3686d0e5b3e53de47ae672aedb3514)
    (cherry picked from commit b135acdb4bc543652486aa11a4b7f46d8eae18dd)

--- agent/server.py
+++ agent/server.py
@@ -231,7 +231,7 @@ class Server(Base):
         directory: str,
         is_primary: bool,
         secondary_server_private_ip: str,
-        redis_password: str,
+        agent_password: str,
         redis_connection_string_ip: str,
         restart_benches: bool = True,
         registry_settings: dict | None = None,
@@ -243,7 +243,7 @@ class Server(Base):
         self._reload_nginx()
 
         self._update_site_config_with_new_rq_conf(
-            redis_connection_string_ip, redis_password
+            redis_connection_string_ip, agent_password
         )  # Update common site config
 
         if restart_benches:
@@ -251,12 +251,12 @@ class Server(Base):
             self.restart_benches(
                 is_primary=is_primary,
                 registry_settings=registry_settings,
-                redis_password=redis_password,
+                agent_password=agent_password,
                 secondary_server_private_ip=secondary_server_private_ip if not is_primary else None,
             )
 
     @step("Configure Site with Redis Private IP")
-    def _update_site_config_with_new_rq_conf(self, private_ip: str, redis_password: str):
+    def _update_site_config_with_new_rq_conf(self, private_ip: str, agent_password: str):
         for _, bench in self.benches.items():
             common_site_config = bench.get_config(for_update=True)
 
@@ -270,7 +270,7 @@ class Server(Base):
                 else:
                     port = 11000 if key == "redis_queue" else 13000
 
-                updated_connection_string = f"redis://:{redis_password}@{private_ip}:{port}"
+                updated_connection_string = f"redis://:{agent_password}@{private_ip}:{port}"
                 common_site_config.update({key: updated_connection_string})
 
             bench.set_config(common_site_config)
@@ -297,14 +297,14 @@ class Server(Base):
         self,
         is_primary: bool,
         secondary_server_private_ip: str,
-        redis_password: str,
+        agent_password: str,
         registry_settings: dict[str, str],
     ):
         if not is_primary:
             # Don't need to pull images on primary server
             self.docker_login(registry_settings)
         for _, bench in self.benches.items():
-            self.update_redis_password(bench, redis_password)  # Update redis conf files
+            self.update_redis_password(bench, agent_password)  # Update redis conf files
             bench.start(secondary_server_private_ip=secondary_server_private_ip)
 
     @job("Stop Bench Workers")
