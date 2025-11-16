106031bf569308ff99104e9100ed9f2615d84561
Aradhya-Tripathi <aradhya@frappe.io>
2025-11-16 09:09:37 +0000

    chore(monitor): Just look for idle benches, if access log is not found assume bench is not prepared

--- monitor_idle.py
+++ monitor_idle.py
@@ -1,94 +1,111 @@
 #!/usr/bin/env python
+# Inform master in case a bench has not servered any request for
+# the last <idle_threshold> duration; In case access log is not found
+# assume that the bench is being prepared
+
 from __future__ import annotations
 
 import json
 import os
 import sys
 import time
+from dataclasses import dataclass, field
 
 import requests
 
-if "--agent-directory" in sys.argv:
-    AGENT_DIRECTORY = sys.argv[sys.argv.index("--agent-directory") + 1]
-else:
-    print("Error: --agent-directory argument required")
-    sys.exit(1)
 
-CHECK_INTERVAL = 5
-IDLE_THRESHOLD = 300  # After which the bench will be requested to die
+@dataclass
+class IdleMonitor:
+    agent_directory: str
+    check_interval: int = 5
+    idle_threshold: int = 300
+    shared_benches_directory: str = "/home/frappe/shared"
+    config: dict[str] = field(default_factory=dict)
 
+    def __post_init__(self):
+        self.config = self.get_config()
 
-def get_agent_config() -> dict[str, str | int]:
-    """Get agent configuration"""
-    with open(os.path.join(AGENT_DIRECTORY, "config.json")) as agent_config:
-        return json.loads(agent_config.read())
+    def get_config(self) -> dict[str, str | int]:
+        """Read only agent config"""
+        with open(os.path.join(self.agent_directory, "config.json")) as agent_config:
+            return json.loads(agent_config.read())
 
+    def check_idle_slave(self, bench_path: str) -> bool:
+        """Check if the modified time of the gunicorn access log is older than IDLE_THRESHOLD"""
+        server_name = self.config["name"]
+        access_log = os.path.join(bench_path, "logs", f"{server_name}-gunicorn.access.log")
+        if not os.path.exists(access_log):
+            # We might be waiting to recieve requests from proxy
+            # We might also be waiting for a new bench job to finish
+            return False
 
-def check_idle_slave(bench_path: str, server_name: str) -> bool:
-    """Check if the modified time of the gunicorn access log is older than IDLE_THRESHOLD"""
-    access_log = os.path.join(bench_path, "logs", f"{server_name}-gunicorn.access.log")
-    if not os.path.exists(access_log):
-        return True
+        current_time = time.time()
+        last_modified = os.stat(access_log).st_mtime
+        idle_time = current_time - last_modified
 
-    current_time = time.time()
-    last_modified = os.stat(access_log).st_mtime
-    idle_time = current_time - last_modified
+        return idle_time > self.idle_threshold
 
-    return idle_time > IDLE_THRESHOLD
+    def inform_master(self) -> None:
+        """Let the master know of idle benches"""
+        try:
+            requests.post(
+                f"{self.config['press_url']}/api/method/press.api.server.benches_are_idle",
+                data={"server": self.config["name"], "access_token": self.config["access_token"]},
+                timeout=10,
+            )
+            print(f"Informed master at {self.config['press_url']} that benches are idle")
+        except Exception as e:
+            print(f"Error informing master: {e}")
 
+    @property
+    def should_monitor(self):
+        """Ensure this never runs on a primary server as it kills slaves"""
+        name = self.config["name"]
 
-def inform_master(press_url: str, config: dict) -> None:
-    """Let the master know of idle benches"""
-    try:
-        requests.post(
-            f"{press_url}/api/method/press.api.server.benches_are_idle",
-            data={"server": config["name"], "access_token": config["access_token"]},
-            timeout=10,
-        )
-        print(f"Informed master at {press_url} that benches are idle")
-    except Exception as e:
-        print(f"Error informing master: {e}")
+        if not name.startswith("fs"):
+            print("Executing monitor on primary server is not allowed")
+            sys.exit(1)
 
+    def monitor(self, force: bool = False) -> None:
+        if not force and not self.should_monitor:
+            print("Can not run monitor on a primary server!")
+            sys.exit(1)
 
-def main() -> None:
-    """Check if all benches are idle and let the master know"""
-    config = get_agent_config()
-    benches_are_idle = False
-    benches_directory = "/home/frappe/shared"  # This is where benches are on secondary server!
-    benches = list(os.scandir(benches_directory))
+        benches_are_idle = False
+        benches_directory = "/home/frappe/shared"  # This is where benches are on secondary server!
+        benches = list(os.scandir(benches_directory))
 
-    for bench in benches:
-        if not bench.name.startswith("bench-"):
-            continue
+        while True:
+            for bench in benches:
+                if not bench.name.startswith("bench-"):
+                    continue
 
-        is_idle = check_idle_slave(bench.path, config["name"])
-        if not is_idle:
-            print(f"Bench {bench.name} is still active")
-            benches_are_idle = False
-            break
+                is_idle = self.check_idle_slave(bench.path)
+                if not is_idle:
+                    print(f"Bench {bench.name} is active")
+                    benches_are_idle = False
+                    break
 
-        print(f"Bench {bench.name} is idle")
-        benches_are_idle = True
+                print(f"Bench {bench.name} is idle")
+                benches_are_idle = True
 
-    if benches_are_idle:
-        inform_master(config["press_url"], config)
-    else:
-        print("Not all benches are idle, skipping master notification")
+            if benches_are_idle:
+                self.inform_master()
+            else:
+                print("Not all benches are idle, skipping master notification")
 
-
-def verify_if_monitor_should_run():
-    """Ensure this never runs on a primary server as it kills slaves"""
-    config = get_agent_config()
-    name = config["name"]
-
-    if not name.startswith("fs"):
-        print("Executing monitor on primary server is not allowed")
-        sys.exit(1)
+            time.sleep(self.check_interval)
 
 
 if __name__ == "__main__":
-    # verify_if_monitor_should_run()
+    if "--agent-directory" in sys.argv:
+        agent_directory = sys.argv[sys.argv.index("--agent-directory") + 1]
+    else:
+        print("No agent directory supplied!")
+        sys.exit(1)
 
-    while True:
-        main()
-        time.sleep(CHECK_INTERVAL)
+    IdleMonitor(
+        agent_directory=agent_directory,
+        check_interval=5,
+        idle_threshold=300,
+    ).monitor(force=True)
