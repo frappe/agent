b2dd2f2f63c4a281058b07ccdaea7b7ee2dea47b
20vikash <thisisforspotifyvikash@gmail.com>
2026-05-04 15:44:13 +0000

    feat(agent): Sign payload in functions before calling press

--- agent/callbacks.py
+++ agent/callbacks.py
@@ -1,8 +1,20 @@
 import requests
 
+from agent.utils import generate_agent_token
+
 
 def callback(job, connection, result, *args, **kwargs):
     from agent.server import Server
 
-    press_url = Server().press_url
-    requests.post(url=f"{press_url}/api/method/press.api.callbacks.callback", data={"job_id": job.id})
+    server = Server()
+    press_url = server.press_url
+
+    path = "/api/method/press.api.callbacks.callback"
+    data = {"job_id": job.id}
+    token = generate_agent_token(data, "POST", path)
+
+    requests.post(
+        url=f"{press_url}{path}",
+        data=data,
+        headers={"X-Agent-Token": token},
+    )
