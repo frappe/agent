5e90e6842165465cc5f1481430d82ea591da49b7
regdocs <55305804+regdocs@users.noreply.github.com>
2026-05-15 13:59:47 +0530

    fix(restore-with-files): Receive sanitized site config
    (cherry picked from commit e64d1d4bac020cf5a5a17fb9104d2b166bd20015)

--- agent/web.py
+++ agent/web.py
@@ -583,7 +583,7 @@ def restore_site(bench, site):
             data["database"],
             data.get("public"),
             data.get("private"),
-            data.get("config"),
+            data.get("sanitized_config_content"),
             data.get("skip_failing_patches", False),
         )
     )
